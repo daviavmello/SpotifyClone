c72c5a667761ae898f5bc5c7aa737c8a46d0b038
Davi Mello <davimello1994@gmail.com>
2021-04-19 21:52:57 -0400

    updating ui

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/data/other/Constants.kt
@@ -0,0 +1,9 @@
+package com.plcoding.spotifycloneyt.data.other
+
+object Constants {
+
+    const val SONG_COLLECTION = "songs"
+
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
+}
