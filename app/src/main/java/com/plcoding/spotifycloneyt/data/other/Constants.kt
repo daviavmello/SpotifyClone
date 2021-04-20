4db4775f465cd2ef1787be179f9aca60d78fe1c6
SmithDevEndeavor <SmithDevEndeavor@gmail.com>
2021-04-20 14:39:29 -0400

    updated ui.fragments, ui.viewmodels, adapters packages and updates to exoplayer.callbacks and data.others packages

--- app/src/main/java/com/plcoding/spotifycloneyt/data/other/Constants.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/data/other/Constants.kt
@@ -4,6 +4,10 @@ object Constants {
 
     const val SONG_COLLECTION = "songs"
 
+    const val MEDIA_ROOT_ID = "root_id"
+
+    const val NETWORK_ERROR = "NETWORK_ERROR"
+
     const val NOTIFICATION_CHANNEL_ID = "music"
     const val NOTIFICATION_ID = 1
 }
